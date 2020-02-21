1b3ec52b5f1bd903e9a74758ac54e9a30422c8f8
kyamagucUHM <35577340+kyamagucUHM@users.noreply.github.com>
2020-02-21 08:33:25 -1000

    Initial commit

---
+++ Assets/Scripts/enemyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyController : MonoBehaviour {
+    public float moveSpeed = 50.0f;
+    public GameObject fireWall;
+    private Transform target;
+
+    
+    // Use this for initialization
+    void Start () {
+        target = GameObject.FindGameObjectWithTag("fireWall").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = transform.position;
+        pos.x += moveSpeed * (target.position.x - pos.x) * Time.deltaTime;
+        pos.y += moveSpeed * (target.position.y - pos.y) * Time.deltaTime;
+        transform.position = pos;
+    } 
+}
---
+++ Assets/Scripts/spawnScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnScript : MonoBehaviour {
+
+    public GameObject enemyPrefab;
+    public Transform spawnPosition;
+    public int count = 0;
+    bool wallExist = true;
+
+    void spawnTank()
+    {
+        GameObject tank = Instantiate(enemyPrefab) as GameObject;
+        tank.transform.position = spawnPosition.position;
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (count < 1)
+        {
+            Invoke("spawnTank", 5.0f);
+            count += 1;
+        }
+	}
+}
