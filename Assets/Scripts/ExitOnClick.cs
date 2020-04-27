4d8b69a0e8f5bf8f40bf166ac6b0f1784656d05c
kyamagucUHM <35577340+kyamagucUHM@users.noreply.github.com>
2020-04-26 15:13:44 -1000

    Update ExitOnClick.cs
    Removed Conflicts

--- Assets/Scripts/ExitOnClick.cs
+++ Assets/Scripts/ExitOnClick.cs
@@ -1,25 +1,9 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ExitOnClick : MonoBehaviour {
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     public void ExitGame()
     {
         Application.Quit();
