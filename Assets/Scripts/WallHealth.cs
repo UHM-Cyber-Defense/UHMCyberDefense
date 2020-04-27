6da0af49518e4f0661715e945e4c14717e54aea5
kyamagucUHM <35577340+kyamagucUHM@users.noreply.github.com>
2020-04-26 15:19:19 -1000

    Update and rename wallHealth.cs to WallHealth.cs
    Removed Conflicts

--- Assets/Scripts/WallHealth.cs
+++ Assets/Scripts/WallHealth.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,21 +6,8 @@ using UnityEngine.UI;
 public class WallHealth : MonoBehaviour {
 
     public GameObject fireWall;
-<<<<<<< Updated upstream
-    public int Health;
-    public Text healthText;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    GameObject[] GameOverObjects;
-    // Use this for initialization
-    void Start () {
-=======
-=======
->>>>>>> Stashed changes
-=======
     public Slider healthBar;
     public static int Health;
->>>>>>> Stashed changes
     private ScoreBoard gManager;
     GameObject[] GameOverObjects;
     // Use this for initialization
@@ -34,13 +21,6 @@ public class WallHealth : MonoBehaviour {
         {
             Debug.Log("Cannot find 'ScoreBoard' script");
         }
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
         Health = 500;
         UpdateHealth();
         GameOverObjects = GameObject.FindGameObjectsWithTag("GameOver");
@@ -49,28 +29,14 @@ public class WallHealth : MonoBehaviour {
 
     void Update()
     {
-<<<<<<< Updated upstream
-=======
         healthBar.value = Health;
->>>>>>> Stashed changes
         if (Health <= 0)
         {
             Destroy(gameObject);
             if (Time.timeScale == 1)
             {
                 Time.timeScale = 0;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
                 gManager.SaveScoreboard();
->>>>>>> Stashed changes
-=======
-                gManager.SaveScoreboard();
->>>>>>> Stashed changes
-=======
-                gManager.SaveScoreboard();
->>>>>>> Stashed changes
                 ShowGameOver();
             }
         }
@@ -100,10 +66,6 @@ public class WallHealth : MonoBehaviour {
 
     void UpdateHealth()
     {
-<<<<<<< Updated upstream
-        healthText.text = "Firewall: " + Health;
-=======
         healthBar.value = Health;
->>>>>>> Stashed changes
     }
 }
